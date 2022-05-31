4fad16e8b98ae8c273beaf12cb077dbd3f1e1e29
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-05-31 10:16:52 -0700

    luxon DateTime defined to currentDay, clockDisplay set to currentDay DATETIME_FULL

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,23 @@
+// import luxon from "https://cdnjs.cloudflare.com/ajax/libs/luxon/2.4.0/luxon.min.js";
+var DateTime = luxon.DateTime;
+// var dt = DateTime.fromISO(new Date().toISOString());
+// var day = (dt.toFormat('EEEE, MMMM YYYY'));
+// var time = (dt.toFormat('hh:mm a'));
+// var currentTime = dt.toISO();
+
+
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
+function init(){
+    // THEN the current day is displayed at the top of the calendar
+}
+var clockDisplay = document.getElementById('currentDay');
+var currentTime = DateTime.local().toLocaleString(DateTime.DATETIME_FULL);
+// this.localDateTime = DateTime.local().toLocaleString(DateTime.DATETIME_FULL);
+console.log(currentTime);
+clockDisplay.textContent = currentTime;
+// $("#currentDay").html(currentTime);
 
-
-// THEN the current day is displayed at the top of the calendar
 // WHEN I scroll down
 // THEN I am presented with time blocks for standard business hours
 // WHEN I view the time blocks for that day

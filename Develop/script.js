8dd092b162a6299bc4fc5a627fb21b8725ecb7c9
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-06-18 01:49:11 -0700

    debugged present, past, future error, stylesheet was overriding script added class so had to use inherent property for textspace, Luxon working fine

--- Develop/script.js
+++ Develop/script.js
@@ -38,33 +38,36 @@ function clockReset(){
 // THEN the text for that event is saved in local storage
 // WHEN I refresh the page
 // THEN the saved events persist
-$("hour9 .textSpace").val(localStorage.getItem("hour9"));
-$("hour10 .textSpace").val(localStorage.getItem("hour10"));
-$("hour11 .textSpace").val(localStorage.getItem("hour11"));
-$("hour12 .textSpace").val(localStorage.getItem("hour12"));
-$("hour1 .textSpace").val(localStorage.getItem("hour1"));
-$("hour2 .textSpace").val(localStorage.getItem("hour2"));
-$("hour3 .textSpace").val(localStorage.getItem("hour3"));
-$("hour4 .textSpace").val(localStorage.getItem("hour4"));
-$("hour5 .textSpace").val(localStorage.getItem("hour5"));
+$("#hour9 .textSpace").val(localStorage.getItem("hour9"));
+$("#hour10 .textSpace").val(localStorage.getItem("hour10"));
+$("#hour11 .textSpace").val(localStorage.getItem("hour11"));
+$("#hour12 .textSpace").val(localStorage.getItem("hour12"));
+$("#hour1 .textSpace").val(localStorage.getItem("hour1"));
+$("#hour2 .textSpace").val(localStorage.getItem("hour2"));
+$("#hour3 .textSpace").val(localStorage.getItem("hour3"));
+$("#hour4 .textSpace").val(localStorage.getItem("hour4"));
+$("#hour5 .textSpace").val(localStorage.getItem("hour5"));
 // the values stored as setItem to hour.textSpace objects are retrieved upon reload
 
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future
 // for each row of the hour - find the present hour
 $('.row').each(function() {
-    var presentRow = parseInt($(this).attr('id').split('-')[1]);
-    console.log(presentRow);
+    var presentRow = parseInt($(this).attr('id').replace('hour', ''));
+    var curr = luxon.DateTime.local().hour
+
     // if present == addClass .present
-    if (currentTime === presentRow) {
-        $(this).addClass('.present')
+    if (curr === presentRow) {
+        $(this).addClass('present')
         // else if > .past
-    } else if (currentTime > presentRow) {
-        $(this).addClass('.past')
+    } else if (curr > presentRow) {
+        $(this).addClass('past')
     } else {
-        $(this).addClass('.future')
+        $(this).addClass('future')
     }
 });
 });
+
+
 
 // WHEN I scroll down
 // THEN I am presented with time blocks for standard business hours
